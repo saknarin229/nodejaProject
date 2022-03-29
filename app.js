00c72486b6bda733336831efb21b0b3cabe9d668
saknarin229 <saknarin229@gmail.com>
2022-03-30 01:28:32 +0700

    main

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var layout = require('express-layout');
 
 var indexRouter = require('./routes/Home/index');
-var usersRouter = require('./routes/User/users');
+
 
 var app = express();
 
@@ -24,7 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/Home/index.js
+++ routes/Home/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('Home/Home', { title: 'Express' });
+    res.render('Home/Home', { Title: 'DaveToDave : รับทำเว็บไซต์พร้อมใช้งาน ดำเนินการให้ทั้งหมด' });
 });
+
+router.get('/contact', function(req, res, next) {
+    res.render('Home/Contact', { Title: 'Contact : รับทำเว็บไซต์พร้อมใช้งาน ดำเนินการให้ทั้งหมด' });
+})
 
 module.exports = router;
